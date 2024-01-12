e59a2e09d7cc5509e16f514171d8c0e4efa40327
Mednourkahouli <148196974+Mednourkahouli@users.noreply.github.com>
2024-01-12 02:08:51 +0100

    Working on home page

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Technofix</title>
+    <style>
+    body {
+        background-image: url(a87678fc20365ff68fef33ac08adbbb8.jpg);
+    }
+    ul {
+  list-style-type: none;
+  margin: 0px;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+div {
+  border: 1px solid gray;
+  padding: 8px;
+}
+
+h1 {
+  text-align: center;
+  text-transform: uppercase;
+  color: #ff0000;
+}
+
+p {
+  text-indent: 50px;
+  text-align: justify;
+  letter-spacing: 3px;
+}
+
+a {
+  text-decoration: none;
+  color: #008CBA;
+}
+img {
+  border-radius: 50%;
+}
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+    </style>
+</head>
+<body>
+    <ul>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+      <div>
+      <h1> Our repair phone shop is a specialized establishment that offers services to fix or restore malfunctioning or damaged mobile phones. These shops employ technicians skilled in diagnosing and repairing various phone issues, including screen replacements, battery replacements, software troubleshooting, and hardware repairs, ensuring the phones function properly again. </h1>
+      <div class="row">
+        <div class="column">
+          <img src="Images\R.jpeg"  style="width:100%">
+        </div>
+        <div class="column">
+          <img src="34078_hd.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+          <img src="OIP (1).jpeg"  style="width:100%">
+        </div>
+      </div>
+
+    </div>
+</body>
+</html>
