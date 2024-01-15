3019fae5833d8222f30f497875f34a07ff6fa0cd
walidtamboura <walid.tamboura2@gmai.com>
2024-01-15 19:26:52 +0100

    contact.html

---
+++ services.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>services</title>
+    <style>
+        body {
+        background-color:lightblue
+    }
+    ul {
+  list-style-type: none;
+  margin: 0px;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+div {
+  border: 1px solid gray;
+  padding: 8px;
+}
+
+h1 {
+  text-align: center;
+  text-transform: uppercase;
+  color: #ff0000;
+}
+
+p {
+  text-indent: 50px;
+  text-align: justify;
+  letter-spacing: 3px;
+}
+
+a {
+  text-decoration: none;
+  color: #008CBA;
+}
+img {
+  border-radius: 50%;
+}
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+    </style>
+</head>
+<body>
+    <ul>
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="services.php">services</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+      <h1> Our shop is specialized in : 
+        <br> 
+        Device Repair: These shops can fix hardware and software issues on electronic devices. This may include screen replacements, battery replacements, fixing water damage, repairing charging ports, and resolving software issues.
+<br>
+Troubleshooting and Diagnostics: Technicians at these shops are often skilled in diagnosing problems with electronic devices. They can identify issues, provide estimates for repairs, and offer solutions.
+<br>
+Upgrades: Some fixing technology shops offer hardware upgrades for devices, such as increasing RAM or storage capacity in laptops, upgrading graphics cards in gaming consoles, or installing new components in desktop computers.
+<br>
+Data Recovery: In cases of data loss due to hardware failure or accidental deletion, these shops may offer data recovery services to retrieve important files and information.
+<br>
+Software Installation and Configuration: Technicians can assist customers with installing or updating software, configuring settings, and ensuring that devices are running the latest operating systems and applications.
+<br>
+Customization: Some fixing technology shops may offer customization services, allowing customers to personalize their devices with accessories, skins, or modifications.
+<br>
+These shops play a crucial role in extending the lifespan of electronic devices and reducing electronic waste by providing cost-effective solutions for repairs rather than replacing entire devices. Customers often turn to fixing technology shops when their devices encounter problems, seeking professional help to get them back in working order.</h1>
+<br>
+ <h1>We fix all of these : </h1>
+        <div class="column">
+          <img src="1.jpeg"  style="width:100%">
+        </div>
+        <div class="column">
+          <img src="2.jpeg"  style="width:100%">
+        </div>
+        <div class="column">
+          <img src="3.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+          <img src="4.jpeg"  style="width:100%">
+        </div>
+      </div>
+    </div>
+    <h1> Our Techniciens: </h1>
+        <div class="column">
+          <img src="tech1.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+          <img src="tech2.jpeg"  style="width:100%">
+        </div>
+        <div class="column">
+          <img src="tech3.jpeg"  style="width:100%">
+        </div>
+        <div class="column">
+          <img src="tech4.jpg"  style="width:100%">
+        </div>
+</body>
+</html>
