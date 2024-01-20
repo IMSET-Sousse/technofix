f9ecc559cf49b2e1d4b754405b6f2aafc92dbf8e
Mednourkahouli <nourkahouli13@gmail.com>
2024-01-20 09:30:33 +0100

    updates

--- contact.php
+++ contact.php
@@ -1,223 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Required meta tags --> 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link href="./contact.css" rel="stylesheet">
+<!--external css-->
+<link href="contact.css" rel="stylesheet">
+<!--bootstrap 5 icon-->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-<title>Contact Us</title>
-<style>
-    ul {
-  list-style-type: none;
-  margin: 0px;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
+<title>Contact form</title>
 
-li {
-  float: left;
-}
-
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-li a:hover {
-  background-color: #111;
-}
-div {
-  border: 0px solid gray;
-  padding: 8px;
-}
-      *,
-*:before,
-*:after{
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-}
-body{
-    background-color: lightblue;
-}
-.background{
-    width: 430px;
-    height: 520px;
-    position: absolute;
-    transform: translate(-50%,-50%);
-    left: 50%;
-    top: 50%;
-}
-.background .shape{
-    height: 200px;
-    width: 200px;
-    position: absolute;
-    border-radius: 50%;
-}
-.shape:first-child{
-    background: linear-gradient(
-        #1845ad,
-        #23a2f6
-    );
-    left: -80px;
-    top: -80px;
-}
-.shape:last-child{
-    background: linear-gradient(
-        to right,
-        #ff512f,
-        #f09819
-    );
-    right: -30px;
-    bottom: -80px;
-}
-form{
-    height: 520px;
-    width: 400px;
-    background-color: rgba(255,255,255,0.13);
-    position: absolute;
-    transform: translate(-50%,-50%);
-    top: 50%;
-    left: 50%;
-    border-radius: 10px;
-    backdrop-filter: blur(10px);
-    border: 2px solid rgba(255,255,255,0.1);
-    box-shadow: 0 0 40px rgba(8,7,16,0.6);
-    padding: 50px 35px;
-}
-form *{
-    font-family: 'Poppins',sans-serif;
-    color: #ffffff;
-    letter-spacing: 0.5px;
-    outline: none;
-    border: none;
-}
-form h3{
-    font-size: 32px;
-    font-weight: 500;
-    line-height: 42px;
-    text-align: center;
-}
-
-label{
-    display: block;
-    margin-top: 30px;
-    font-size: 16px;
-    font-weight: 500;
-}
-input{
-    display: block;
-    height: 50px;
-    width: 100%;
-    background-color: rgba(255,255,255,0.07);
-    border-radius: 3px;
-    padding: 0 10px;
-    margin-top: 8px;
-    font-size: 14px;
-    font-weight: 300;
-}
-::placeholder{
-    color: #e5e5e5;
-}
-button{
-    margin-top: 50px;
-    width: 100%;
-    background-color: #ffffff;
-    color: #080710;
-    padding: 15px 0;
-    font-size: 18px;
-    font-weight: 600;
-    border-radius: 5px;
-    cursor: pointer;
-}
-.social{
-  margin-top: 30px;
-  display: flex;
-}
-.social div{
-  background: red;
-  width: 150px;
-  border-radius: 3px;
-  padding: 5px 10px 10px 5px;
-  background-color: rgba(255,255,255,0.27);
-  color: #eaf0fb;
-  text-align: center;
-}
-.social div:hover{
-  background-color: rgba(255,255,255,0.47);
-}
-.social .fb{
-  margin-left: 25px;
-}
-.social i{
-  margin-right: 4px;
-}
-*{
-padding: 0; 
-margin: 0; 
-box-sizing: border-box;
-}
-
-.container{ width: 60%;
-     padding: 40px; 
-     border-radius: 0; 
-     position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%,-50%)
-}
-.row{
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-}
-col-md-7{
-    padding: 20px;
-}
-
-    .col-md-5{
-    background-color:rgb(53, 53, 247);
-    padding: 20px;
-    color:white;
-}
-.form-control{ 
-    height: 52px;
-     background:#fff;
-color:#000;
-font-size: 14px;
-border-radius: 2px;
-box-sizing: none !important ;
-border: 1px solid rgba(0, 0, 0, 0.1);
-}
-    .bi{font size: 20px;
-}
-.d-flex p{
-font-size: 18px; 
-padding-left: 18px;
-font-family: Verdana, Geneva, Tahoma, sans-serif;
-}
-
-@media only screen and(max-width:60px){
-    .container~{
-        width: 100%;
-        position: absolute;
-        left: 50%;
-        top: 80%;
-    }
-}
-</style>
 </head>
 <body>
-<ul>
-    <li><a href="login.php">login-register</a></li>
-        <li><a class="active" href="index.php">Home</a></li>
-        <li><a href="services.php">services</a></li>
-        <li><a href="reviews.php">reviews</a></li>
-        <li><a href="contact.php">contact us</a></li>
-      </ul>
     <div class="container">
         <div class="row">
             <div class="col-md-7">
@@ -271,6 +68,7 @@ font-family: Verdana, Geneva, Tahoma, sans-serif;
     
             <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <img src="1111.jpg" alt="Girl in a jacket" width="500" height="300">
 </body>
 
 </html>
